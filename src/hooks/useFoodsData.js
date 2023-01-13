e561347613f4590278c9ffe298ac532baa93bd6d
Shivam Shukla <shivam@gkmit.co>
2023-01-13 11:05:06 +0530

    create form to add food item

--- src/hooks/useFoodsData.js
+++ src/hooks/useFoodsData.js
@@ -1,4 +1,4 @@
-import { useQuery } from 'react-query';
+import { useQuery, useMutation } from 'react-query';
 import axios from 'axios';
 
 const fetchFoodData = () => {
@@ -9,4 +9,18 @@ export const useFoodsData = () => {
   return useQuery('foods-data', fetchFoodData, {
     select: (data) => data.data.data.map((food) => food)
   });
+};
+
+const addFood = (data) => {
+  console.log({ data });
+  return axios.post(`http://localhost:1337/api/foods`, data, {
+    headers: {
+      'Content-Type': 'multipart/form-data',
+      Authorization: `Bearer ${import.meta.env.VITE_API_TOKEN}`
+    }
+  });
+};
+
+export const useAddFoodData = () => {
+  return useMutation(addFood);
 };
